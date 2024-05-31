551943de245a6fb33b8ffb4ac8bc6900a7668916
akhalim1 <156042742+akhalim1@users.noreply.github.com>
2024-05-31 15:19:50 -0700

    fixing checkpoint animator

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,14 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private Transform player1SpawnPoint, player2SpawnPoint;
+
     private bool isActive;
     public Animator anim;
 
     void OnTriggerEnter2D(Collider2D collideer){
         if(collideer.gameObject.tag == "Player" && isActive == false){
             RespawnManager.instance.setSpawnPoint(player1SpawnPoint.position, player2SpawnPoint.position);
-            anim.SetBool("flagActive", true);
+            anim.SetBool("flagActive", true); 
 
             isActive = true;
         }

3b6b4131e4243dac2f76c2d62cca1cd709508a73
scso-ucsc <scso@ucsc.edu>
2024-05-30 21:08:40 -0700

    Updating Timer Script to use GameManager

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,15 +6,13 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
+    private float remainingTime;
     // Update is called once per frame
     void Update()
     {
-        remainingTime -= Time.deltaTime;
+        remainingTime = GameManager.instance.getPlayerTime();
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-
-        
     }
 }

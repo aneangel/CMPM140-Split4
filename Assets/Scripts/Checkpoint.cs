c2366b213c184e3ecc9338b5fe0cf7ace5f938e0
akhalim1 <156042742+akhalim1@users.noreply.github.com>
2024-05-31 14:59:58 -0700

    adding checkpoint anims

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private Transform player1SpawnPoint, player2SpawnPoint;
+    private bool isActive;
+    public Animator anim;
 
     void OnTriggerEnter2D(Collider2D collideer){
-        if(collideer.gameObject.tag == "Player"){
+        if(collideer.gameObject.tag == "Player" && isActive == false){
             RespawnManager.instance.setSpawnPoint(player1SpawnPoint.position, player2SpawnPoint.position);
+            anim.SetBool("flagActive", true);
+
+            isActive = true;
         }
     }
 }
